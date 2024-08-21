767ec0c561486b6b7778c8f6c4619aa5fd8606ee
kikichechka <90552053+kikichechka@users.noreply.github.com>
2024-08-22 02:03:28 +0300

    create layout search card

--- input/src/main/java/com/example/jobsearch/input/LogAccountFragment.kt
+++ input/src/main/java/com/example/jobsearch/input/LogAccountFragment.kt
@@ -1,10 +1,16 @@
 package com.example.jobsearch.input
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnFocusChangeListener
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
 import com.example.jobsearch.input.databinding.FragmentLogAccountBinding
 
 class LogAccountFragment : Fragment() {
@@ -18,6 +24,36 @@ class LogAccountFragment : Fragment() {
     ): View {
         _binding = FragmentLogAccountBinding.inflate(inflater)
         return binding.root
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.edit.setCompoundDrawablesRelativeWithIntrinsicBounds(
+            resources.getDrawable(R.drawable.letter, null), null, null, null
+        )
+
+        binding.edit.onFocusChangeListener = OnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                binding.edit.hint = null
+                binding.layoutEdit.hint = null
+                binding.edit.setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, null, null)
+            }
+        }
+
+        binding.edit.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.buttonContinue.isEnabled = !p0.isNullOrBlank()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
     }
 
     override fun onDestroy() {
