38ba53c360f991f4fc425073f0f534ac5d6ca054
kikichechka <90552053+kikichechka@users.noreply.github.com>
2024-08-21 17:03:12 +0300

    The main activity markup and navigation have been created

---
+++ app/src/main/java/com/example/jobsearch/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.jobsearch
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.jobsearch.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val badge = binding.bottomNavigation.getOrCreateBadge(R.id.favouritesFragment)
+        badge.isVisible = true
+        badge.number = 1
+    }
+}
