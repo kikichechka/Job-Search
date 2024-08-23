f8e7fda2ff6647fb3fd9e21574d4b2021527557a
kikichechka <90552053+kikichechka@users.noreply.github.com>
2024-08-24 01:41:40 +0300

    markup SearchFragment and item recommendation is ready

--- search/src/main/java/com/example/jobsearch/search/SearchFragment.kt
+++ search/src/main/java/com/example/jobsearch/search/SearchFragment.kt
@@ -1,17 +1,35 @@
 package com.example.jobsearch.search
 
+import android.R
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.jobsearch.search.databinding.FragmentSearchBinding
+
 
 class SearchFragment : Fragment() {
+    private var _binding: FragmentSearchBinding? = null
+    private val binding: FragmentSearchBinding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    ): View {
+        _binding = FragmentSearchBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val catPlural = this.resources.getQuantityString(com.example.jobsearch.search.R.plurals.count_vacancies, 11, 11)
+        binding.buttonAllVacancies.text = catPlural
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
